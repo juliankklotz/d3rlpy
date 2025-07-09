480bb5c5389d05d9518d4d137074bf68f4bfcacf
juliankklotz <julianklotz1@yahoo.de>
2025-07-09 08:40:14 +0200

    Include logic for removing older models also when evaluating all models

--- d3rlpy/algos/transformer/base.py
+++ d3rlpy/algos/transformer/base.py
@@ -388,8 +388,8 @@ class TransformerAlgoBase(
         eval_action_sampler: Optional[TransformerActionSampler] = None,
         save_interval: int = 1,
         callback: Optional[Callable[[Self, int, int], None]] = None,
-        n_trials: int = 10,
-        eval_gaps: int = 10,
+        n_trials: int = 50,
+        eval_gaps: int = 5,
         patience: int = 10,
     ) -> None:
         """Trains with given dataset.
@@ -532,24 +532,23 @@ class TransformerAlgoBase(
                         if patience_count > patience:
                             exit = True
                     
-                    if eval_gaps != 1:
-                        for kept_model in keep_models[:]:
-                            if kept_model not in range(
-                                best_epoch - 2, best_epoch + 3
-                            ):
-                                try:
-                                    LOG.info(
-                                        f"Removing old model 'd3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3'",
-                                        epoch=kept_model,
-                                    )
-                                    
-                                    os.remove(f"d3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3")
-                                except FileNotFoundError:
-                                    LOG.warning(
-                                        f"Model 'd3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3' not found.",
-                                        epoch=kept_model,
-                                    )
-                                keep_models.remove(kept_model)
+                    
+                    for kept_model in keep_models[:]:
+                        if (eval_gaps != 1 and kept_model not in range(best_epoch - 2, best_epoch + 3)) or \
+                        (eval_gaps == 1 and kept_model != best_epoch):
+                            try:
+                                LOG.info(
+                                    f"Removing old model 'd3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3'",
+                                    epoch=kept_model,
+                                )
+                                
+                                os.remove(f"d3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3")
+                            except FileNotFoundError:
+                                LOG.warning(
+                                    f"Model 'd3rlpy_logs/{logger._experiment_name}/model_epoch_{kept_model}.d3' not found.",
+                                    epoch=kept_model,
+                                )
+                            keep_models.remove(kept_model)
 
 
             # save metrics
